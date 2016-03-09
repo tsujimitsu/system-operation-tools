6ddac1fbb3624a3e810c89e641a981d76863f2ef
tsujimitsu <tsujimitsu@gmail.com>
2016-03-10 01:37:33 +0900

    add selenium sample programm

---
+++ python-scripts/selenium_driver/selenium_driver/selenium_driver.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+
+
+def main():
+
+    driver = webdriver.Ie("C:\work\selenium-IEDriverServer\IEDriverServer.exe")
+    driver.implicitly_wait(10)
+    driver.get("https://google.com/")
+
+
+if __name__ == '__main__':
+    main()

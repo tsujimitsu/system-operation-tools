5a0bb2a3fc3fc22e61f44d15de488517eeb0eb7a
tsujimitsu <tsujimitsu@gmail.com>
2016-03-22 02:11:06 +0900

    update selenium driver code
    get dom element, and print element txt
    capture browser

--- python-scripts/selenium_driver/readme.txt
+++ python-scripts/selenium_driver/readme.txt
@@ -1,3 +1,7 @@
 python selenium driver
 =======================
 
+abstract
+---------
+* run test IE11, Chrome, Firefox
+* dont run test Edge
--- python-scripts/selenium_driver/selenium_driver.py
+++ python-scripts/selenium_driver/selenium_driver.py
@@ -4,12 +4,18 @@
 # http://selenium-release.storage.googleapis.com/index.html
 # http://stackoverflow.com/questions/31134408/unable-to-find-element-on-closed-window-on-ie-11-working-with-selenium
 
+# selenium driver Microsoft Edge
+# default install path: C:\Program Files (x86)\Microsoft Web Driver\
+# https://www.microsoft.com/en-us/download/details.aspx?id=48212
+
 # selenium driver chrome
 # https://chromedriver.storage.googleapis.com/index.html
 
 # reference
 # http://qiita.com/gluelan2013/items/6977cde545e2bcf08081
 # http://miya-jan.hatenablog.com/entry/2014/04/22/232919
+# http://yyyank.blogspot.jp/2015/01/selenium2webdrivertips8.html
+# https://www.qoosky.net/techs/58
 
 
 from selenium import webdriver
@@ -19,15 +25,41 @@ import time
 
 
 def search_to_google(driver, keyword):
+    """Display result of google search
     """
-    """
+    # Access
     driver.get("http://www.google.com/xhtml")
     time.sleep(5)
+    
+    # Search
     search_box = driver.find_element_by_name('q')
     search_box.send_keys(keyword)
     search_box.submit()
     time.sleep(5)
+    
+    # Display result page titles
+    result_titles = driver.find_elements_by_css_selector('.r a')
+    for title in result_titles:
+        print title.text
+    time.sleep(5)
+    
 
+def search_to_yahoo(driver, keyword):
+    """Display result of yahoo search and capture
+    """
+    # Access
+    driver.get("http://www.yahoo.co.jp/")
+    time.sleep(5)
+    
+    # Search
+    search_box = driver.find_element_by_name('p')
+    search_box.send_keys(keyword)
+    search_box.submit()
+    time.sleep(5)
+    
+    # Capture screenshot
+    driver.save_screenshot("./yahoo_search_page.png")
+        
 
 def main():
     """
@@ -35,14 +67,19 @@ def main():
     #driver = webdriver.Firefox()
     #driver = webdriver.Chrome("C:\work\chromedriver_win32\chromedriver.exe")
     
+    # open Edge browser and crash. Maybe Edge driver dont support python.
+    #driver = webdriver.Edge("C:\Program Files (x86)\Microsoft Web Driver\MicrosoftWebDriver.exe")
+    
     # if you use IEdriver, change browser setting
     # Internet Option > Secutiry
     # All security zone "Enable Protected Mode" unchecked.
+    # and change browser display magnification to "100%"
     driver = webdriver.Ie("C:\work\selenium-IEDriverServer\IEDriverServer.exe")
 
 
     driver.implicitly_wait(10)
     search_to_google(driver, u"日本語")
+    search_to_yahoo(driver, u"ニュース")
     
     driver.quit()
     #driver.back()

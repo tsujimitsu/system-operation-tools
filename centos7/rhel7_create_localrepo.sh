67a66222a7d7e0aa0449ba7090f5ec49f27a77cf
tsujimitsu <tsujimitsu@gmail.com>
2017-01-15 18:54:40 +0900

    modified files

--- centos7/rhel7_create_localrepo.sh
+++ centos7/rhel7_create_localrepo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # RHEL7 Create local yum repository
 
+# Use RHN Online Repository
+RH_USERNAME=XXX
+RH_PASSWORD=XXX
+
+
 mount /dev/cdrom /mnt
 mkdir -p /opt/local-repo/rhel-media
 cp -Rp /mnt/* /opt/local-repo/rhel-media
@@ -23,3 +28,29 @@ EOT
 yum clean all
 yum repolist
 yum grouplist
+
+exit 0
+
+
+# Redhat Network
+subscription-manager register --username $RH_USERNAME --password $RH_PASSWORD
+subscription-manager subscribe --auto
+subscription-manager repos --list-enabled
+
+#REPONAME=rhel-ha-for-rhel-7-server-eus-rpms
+#mkdir -p /opt/local-repo/$REPONAME/Packages
+#reposync -r $REPONAME -p /opt/local-repo/$REPONAME/Packages/
+
+yum -y install yum-utils
+
+# RHEL7
+repolist="rhel-ha-for-rhel-7-server-eus-rpms rhel-7-server-eus-rpms rhel-7-server-rpms rhel-ha-for-rhel-7-server-rpms"
+for repo in $repolist; do
+    reposync -r $repo -p /opt/local-repo/
+done
+
+# OpenStack
+repolist="rhel-7-server-openstack-10-tools-rpms rhel-7-server-openstack-9-tools-rpms rhel-7-server-openstack-8-tools-rpms rhel-7-server-openstack-7.0-tools-rpms "
+for repo in $repolist; do
+    reposync -r $repo -p /opt/local-repo/
+done
